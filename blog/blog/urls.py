0e4d31acc4aa7c45d19c7194cea55b1a4e5f53d3
egorik <pochta@gmail.com>
2026-04-21 16:10:39 +0700

    авторизация

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from login import views
 
 urlpatterns = [
+    path('', views.index),
     path('add_user/', views.add_user),
     path('users/', views.users),
+    path('login/', views.login),
+    path('logout/', views.logout),
 ]
--- blog/login/views.py
+++ blog/login/views.py
@@ -1,21 +1,58 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import User
 from .forms import UserForm
 
 
+def index(request):
+    if not request.session.get('user_id'):
+        return redirect('/login/')
+    
+    username = request.session.get('login')
+    return render(request, "base.html", {'username': username})
+
+
 def users(request):
     users = User.objects.all()
-    return render(request, "users.html", {'users' : users})
+    return render(request, "users.html", {'users': users})
 
 
-# Create your views here.
 def add_user(request):
     if request.method == "POST":
-        user = UserForm(request.POST)
-        if user.is_valid():
-            user.save()
-        return redirect('/users/')
-    
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/users/')
     else:
         form = UserForm()
-        return render(request, "add_user.html", {'form' : form})
+
+    return render(request, "add_user.html", {'form': form})
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(login=username).first()
+
+        if not user:
+            messages.error(request, 'Пользователь не найден')
+            return redirect('/login/')
+
+        if password != user.password:
+            messages.error(request, 'Неверный пароль')
+            return redirect('/login/')
+
+        request.session['user_id'] = user.id
+        request.session['login'] = user.login
+
+        return redirect('/')
+
+    return render(request, 'login.html')
+
+
+def logout(request):
+    request.session.flush()
+    messages.info(request, 'Вы вышли из системы')
+    return redirect('/login/')

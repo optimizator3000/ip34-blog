230bda2eb078b92a7bd877567a450ef6c420ae13
egorik <pochta@gmail.com>
2026-04-21 15:21:13 +0700

    добавлена модель пользователя

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -27,6 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INSTALLED_APPS = [
+    #'django.contrib.admin',
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'login',
+]
+
 
 WSGI_APPLICATION = 'blog.wsgi.application'
 

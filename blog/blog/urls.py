f4b12fb0cba905ee65af4bbcea4170c647b2e93f
egorik <pochta@gmail.com>
2026-04-21 15:25:06 +0700

    добавлен маршрут для создания пользователя

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,3 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import path
+from login import views
+
+urlpatterns = [
+    path('add_user/', views.add_user),
+]
